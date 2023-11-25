f0f54902fb7c4d36581dcdb0aad6a21b66937d53
Jesu <117495071+JesuFrancesco@users.noreply.github.com>
2023-11-25 13:09:11 -0500

    sentencias dcl

---
+++ sql/sentencias-dcl.sql
@@ -0,0 +1,22 @@
+CREATE USER INGDATA IDENTIFIED BY 2023; GRANT ALL PRIVILEGES TO INGDATA;
+CREATE USER VETERINARIO IDENTIFIED BY 2023; GRANT ALL PRIVILEGES TO INGDATA;
+CREATE USER RECEPCIONISTA IDENTIFIED BY 2023; GRANT ALL PRIVILEGES TO INGDATA;
+CREATE USER USUARIO IDENTIFIED BY 2023;
+/
+create or replace PROCEDURE grant_select(
+    propietario VARCHAR2, 
+    usuario VARCHAR2)
+AS   
+BEGIN
+    FOR r IN (
+        SELECT owner, table_name 
+        FROM all_tables 
+        WHERE owner = propietario
+    )
+    LOOP
+        EXECUTE IMMEDIATE 
+            'GRANT SELECT ON '||r.owner||'.'||r.table_name||' to ' || usuario;
+    END LOOP;
+END; 
+/
+EXECUTE grant_select('ingdata','usuario');
